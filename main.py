036505dfd206219eea0409142aade0a82180aeb0
Zack <dellaroccozack@gmail.com>
2021-03-25 16:41:01 -0400

    Fixed int division error, and got rid of sounds because they got corrupted.

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ YELLOW = (255, 255, 0)
 BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
 
 # SOUNDS
-BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Grenade+1.mp3'))
-BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Gun+Silencer.mp3'))
+# BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Grenade+1.mp3'))
+# BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Gun+Silencer.mp3'))
 
 # FONT
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
@@ -136,8 +136,8 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
 
 def draw_winner(text):
     draw_text = WINNER_FONT.render(text, 1, WHITE)
-    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
-             2, HEIGHT/2 - draw_text.get_width()/2))
+    WIN.blit(draw_text, (WIDTH//2 - draw_text.get_width() //
+             2, HEIGHT//2 - draw_text.get_width()//2))
 
     pygame.display.update()
     pygame.time.delay(5000)
@@ -171,22 +171,22 @@ def main():
                     bullet = pygame.Rect(
                         yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
-                    BULLET_FIRE_SOUND.play()
+                    # BULLET_FIRE_SOUND.play()
 
                 # RED SHOOT
                 if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(
                         red.x, red.y + red.height//2 - 2, 10, 5)
                     red_bullets.append(bullet)
-                    BULLET_FIRE_SOUND.play()
+                    # BULLET_FIRE_SOUND.play()
 
             if event.type == RED_HIT:
                 red_health -= 1
-                BULLET_HIT_SOUND.play()
+                # BULLET_HIT_SOUND.play()
 
             if event.type == YELLOW_HIT:
                 yellow_health -= 1
-                BULLET_HIT_SOUND.play()
+                # BULLET_HIT_SOUND.play()
 
         winner_text = ""
         if red_health <= 0:
